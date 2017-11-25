204f6e80c9d3b8cf163b739b7e2ade0b61116b96
Steve Aylward <staylward@users.noreply.github.com>
2017-11-25 12:16:32 +1100

    Add answer for example seven

--- src/main/java/com/staylward/stream/dojo/seven/ExampleFilterReduce.java
+++ src/main/java/com/staylward/stream/dojo/seven/ExampleFilterReduce.java
@@ -1,21 +1,10 @@
 package com.staylward.stream.dojo.seven;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class ExampleFilterReduce {
     public static Integer getFilteredTotal(List<Integer> numbers, Predicate<Integer> filter) {
-        List<Integer> filteredNumbers = new ArrayList<>();
-        for (Integer number : numbers) {
-            if (filter.test(number)) {
-                filteredNumbers.add(number);
-            }
-        }
-        Integer total = 0;
-        for (Integer number : filteredNumbers) {
-            total += number;
-        }
-        return total;
+        return numbers.stream().filter(filter).reduce((x, y) -> x+y).orElse(0);
     }
 }
