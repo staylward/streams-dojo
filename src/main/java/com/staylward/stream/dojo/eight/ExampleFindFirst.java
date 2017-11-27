d4cd9ea96e593a97e488c2d4100e05582adf764f
Steve Aylward <staylward@users.noreply.github.com>
2017-11-27 13:11:33 +1100

    Add answer for example eight

--- src/main/java/com/staylward/stream/dojo/eight/ExampleFindFirst.java
+++ src/main/java/com/staylward/stream/dojo/eight/ExampleFindFirst.java
@@ -1,22 +1,11 @@
 package com.staylward.stream.dojo.eight;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class ExampleFindFirst {
 
     public static Optional<Book> findFirstSpecialBook(List<Book> books) {
-        List<Book> booksWithSpecialCharacters = new ArrayList<>();
-        for (Book book : books) {
-            if (book.hasSpecialCharacter()) {
-                booksWithSpecialCharacters.add(book);
-            }
-        }
-        if (booksWithSpecialCharacters.isEmpty()) {
-            return Optional.empty();
-        } else {
-            return Optional.of(booksWithSpecialCharacters.get(0));
-        }
+        return books.stream().filter(Book::hasSpecialCharacter).findFirst();
     }
 }
