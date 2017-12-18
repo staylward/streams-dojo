426a1c2a0d54eadeef2cc0379977f16bbc917492
Steve Aylward <staylward@users.noreply.github.com>
2017-12-18 13:33:58 -0300

    Add answer for example ten

--- src/main/java/com/staylward/stream/dojo/ten/GetMaxInteger.java
+++ src/main/java/com/staylward/stream/dojo/ten/GetMaxInteger.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 public class GetMaxInteger {
     public static Integer getMaximumBy(List<Person> people, String eyeColour) {
-        Integer max = 0;
-        for (Person person : people) {
-            if (person.getEyeColour().equals(eyeColour)) {
-                max = person.getAge() > max ? person.getAge() : max;
-            }
-        }
-        return max;
+        return people.stream()
+                .filter(person -> person.getEyeColour().equals(eyeColour))
+                .mapToInt(Person::getAge)
+                .max()
+                .getAsInt();
     }
 }
