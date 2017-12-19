eedea980e18086b19cecc3fc4a15bceb98281260
Steve Aylward <staylward@users.noreply.github.com>
2017-12-19 14:29:25 -0300

    Add answer for example thirteen

--- src/main/java/com/staylward/stream/dojo/thirteen/ExampleGroupingBy.java
+++ src/main/java/com/staylward/stream/dojo/thirteen/ExampleGroupingBy.java
@@ -1,26 +1,14 @@
 package com.staylward.stream.dojo.thirteen;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ExampleGroupingBy {
     public static Map<Office, List<Employee>> getEmployeesByOffice(List<Employee> employees, Integer requiredYearsInCompany) {
-        Map<Office, List<Employee>> result = new HashMap<>();
-        for (Employee employee : employees) {
-            if (employee.getYearsInCompany() >= requiredYearsInCompany) {
-                Office office = employee.getOffice();
-                if (result.containsKey(office)) {
-                    List<Employee> currentEmployeeList = result.get(office);
-                    currentEmployeeList.add(employee);
-                } else {
-                    List<Employee> newList = new ArrayList<>();
-                    newList.add(employee);
-                    result.put(office, newList);
-                }
-            }
-        }
-        return result;
+        return employees
+                .stream()
+                .filter(employee -> employee.getYearsInCompany() >= requiredYearsInCompany)
+                .collect(Collectors.groupingBy(Employee::getOffice));
     }
 }
