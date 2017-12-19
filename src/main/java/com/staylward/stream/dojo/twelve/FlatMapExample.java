aa23591fb2c3ef608b29f7e51fc58813ccaf0d4b
Steve Aylward <staylward@users.noreply.github.com>
2017-12-19 13:48:03 -0300

    Add answer for example twelve

--- src/main/java/com/staylward/stream/dojo/twelve/FlatMapExample.java
+++ src/main/java/com/staylward/stream/dojo/twelve/FlatMapExample.java
@@ -4,16 +4,12 @@ import java.util.List;
 
 public class FlatMapExample {
     public static Integer calculateTotalPopulation(List<Region> regions, String requestedRegion, Character firstLetterOfTown) {
-        Integer total = 0;
-        for (Region region : regions) {
-            if (region.getName().equals(requestedRegion)) {
-                for (Town town : region.getTowns()) {
-                    if (town.getName().charAt(0) == firstLetterOfTown) {
-                        total += town.getPopulation();
-                    }
-                }
-            }
-        }
-        return total;
+        return regions
+                .stream()
+                .filter(region -> region.getName().equals(requestedRegion))
+                .flatMap(region -> region.getTowns().stream())
+                .filter(town -> town.getName().charAt(0) == firstLetterOfTown)
+                .mapToInt(Town::getPopulation)
+                .sum();
     }
 }
