77168b0c493233a8d9414f6808d268689fe1289f
Steve Aylward <staylward@users.noreply.github.com>
2017-12-19 13:10:29 -0300

    Add answer for example eleven

--- src/main/java/com/staylward/stream/dojo/eleven/CalculateAndPrintDebug.java
+++ src/main/java/com/staylward/stream/dojo/eleven/CalculateAndPrintDebug.java
@@ -5,15 +5,11 @@ import java.util.List;
 public class CalculateAndPrintDebug {
 
     public static Double getMultipliedTotal(List<Item> items, ItemQuality requestedQuality) {
-        Double total = 0d;
-        for (Item item : items) {
-            if (item.getQuality().equals(requestedQuality)) {
-                Double multiplier = item.getMultiplier();
-                Double value = item.getValue();
-                System.out.println(String.format("Processing m:%3.1f v:%3.1f", multiplier, value));
-                total += multiplier * value;
-            }
-        }
-        return total;
+        return items
+                .stream()
+                .filter(item -> item.getQuality().equals(requestedQuality))
+                .peek(item -> System.out.println(String.format("Processing m:%3.1f v:%3.1f", item.getMultiplier(), item.getValue())))
+                .mapToDouble(item -> item.getValue() * item.getMultiplier())
+                .sum();
     }
 }
