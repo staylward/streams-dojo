25c38d956493243563e68bc933d639479302b200
Steve Aylward <staylward@users.noreply.github.com>
2019-01-10 16:00:46 -0300

    Add answer for example fourteen

--- src/main/java/com/staylward/stream/dojo/fourteen/ExampleFilterOutNullValues.java
+++ src/main/java/com/staylward/stream/dojo/fourteen/ExampleFilterOutNullValues.java
@@ -1,20 +1,20 @@
 package com.staylward.stream.dojo.fourteen;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ExampleFilterOutNullValues {
     public static List<String> getAllStudentNames(School school) {
-        List<String> studentNames = new ArrayList<>();
-        for (Teacher teacher : school.getTeachers()) {
-            if (teacher != null) {
-                for (Student student : teacher.getStudents()) {
-                    if (student != null && student.getName() != null) {
-                        studentNames.add(student.getName());
-                    }
-                }
-            }
-        }
-        return studentNames;
+        return school.getTeachers().stream()
+                .filter(Objects::nonNull)
+                .map(Teacher::getStudents)
+                .filter(Objects::nonNull)
+                .flatMap(Collection::stream)
+                .filter(Objects::nonNull)
+                .map(Student::getName)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 }
